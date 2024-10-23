b5bcdc359cec181c72d5f9014b735d960d523bb3
Rene Hdz <mcrohs@hotmail.com>
2024-10-23 21:55:03 +0200

    sin comentarios

--- src/infrastructure/api/controllers/favorite.controller.ts
+++ src/infrastructure/api/controllers/favorite.controller.ts
@@ -1,48 +1,3 @@
-/*import { FavoritePublication } from '../../../usecases/favorite-publication/favorite-publication.usecase';
-import { Request, Response } from 'express';
-import { FavoriteRepositoryPrisma } from '../../repositories/favorite.repository.prisma';
-import { FavoritePublicationDTO } from '../../../usecases/favorite-publication/favorite-publication.dto';
-
-const likeRepository = new FavoriteRepositoryPrisma();
-const likePublicationUseCase = new FavoritePublication(likeRepository);
-
-export const favoritePublicationController = async (req: Request, res: Response) => {
-    try {
-        console.log('Request body:', req.body);
-        const dto: FavoritePublicationDTO = req.body;
-
-        const existingFavorite = await likeRepository.findByUserAndPublication(dto.userId, dto.publicationId);
-
-        if (existingFavorite) {
-            return res.status(400).json({ message: 'Favorite already exists' });
-        }
-
-        await likePublicationUseCase.execute(dto);
-        res.status(201).json({ message: 'Like processed successfully' });
-    } catch (error) {
-        const typedError = error as Error;
-        res.status(400).json({ message: typedError.message });
-    }
-};*/
-
-/*import { Request, Response } from 'express';
-import { FavoriteRepositoryPrisma } from '../../repositories/favorite.repository.prisma';
-
-const favoriteRepository = new FavoriteRepositoryPrisma();
-
-export const favoritePublicationController = async (req: Request, res: Response) => {
-    try {
-        console.log('Request body:', req.body); // Log para verificar el body
-        const { userId, publicationId } = req.body;
-        await favoriteRepository.save({ userId, publicationId, createdAt: new Date() });
-        res.status(201).json({ message: 'Favorite processed successfully' });
-    } catch (error) {
-        const typedError = error as Error;
-        console.error('Error:', typedError.message); // Log del error
-        res.status(400).json({ message: typedError.message });
-    }
-};*/
-
 import { FavoritePublication } from '../../../usecases/favorite-publication/favorite-publication.usecase';
 import { Request, Response } from 'express';
 import { FavoriteRepositoryPrisma } from '../../repositories/favorite.repository.prisma';
@@ -59,7 +14,7 @@ export const favoritePublicationController = async (req: Request, res: Response)
         res.status(201).json({ message: 'Favorite processed successfully' });
     } catch (error) {
         const typedError = error as Error;
-        console.error('Error:', typedError.message); // Log del error
+        console.error('Error:', typedError.message); 
         res.status(400).json({ message: typedError.message });
     }
 };
--- src/infrastructure/repositories/favorite.mapper.ts
+++ src/infrastructure/repositories/favorite.mapper.ts
@@ -1,26 +1,3 @@
-/*import { Favorite as DomainFavorite } from '../../domain/entities/favorite.entity';
-import { Favorite as PrismaFavorite } from '@prisma/client';
-
-export class FavoriteMapper {
-    static toPersistence(favorite: DomainFavorite): PrismaFavorite {
-        return {
-            id: favorite.id,
-            userId: favorite.userId,
-            publicationId: favorite.publicationId,
-            createdAt: favorite.createdAt
-        };
-    }
-
-    static toDomain(prismaFavorite: PrismaFavorite): DomainFavorite {
-        return DomainFavorite.with({
-            id: prismaFavorite.id,
-            userId: prismaFavorite.userId,
-            publicationId: prismaFavorite.publicationId,
-            createdAt: prismaFavorite.createdAt
-        });
-    }
-}*/
-
 import { Favorite as DomainFavorite } from '../../domain/entities/favorite.entity';
 import { Favorite as PrismaFavorite } from '@prisma/client';
 
