b60d750485285e9efbdd86b25c9d9f407a81098a
Rene Hdz <mcrohs@hotmail.com>
2024-11-01 10:20:59 +0100

    borrar

--- src/infrastructure/api/controllers/user.controller.ts
+++ src/infrastructure/api/controllers/user.controller.ts
@@ -39,9 +39,11 @@ import { UserRepositoryPrisma } from '../../repositories/user.repository.prisma'
 import { RegisterUserDTO } from '../../../usecases/users/register-user/register-user.dto';
 import { ListUsers } from '../../../usecases/users/listUsers/list-users.usecase';
 import { ListUsersDTO } from '../../../usecases/users/listUsers/list-users.dto';
+
 const userRepository = new UserRepositoryPrisma();
 
 const registerUserUseCase = new RegisterUser(userRepository);
+
 export const registerUserController = async (req: Request, res: Response) => {
     try {
         const dto: RegisterUserDTO = req.body;
--- src/infrastructure/api/express/routes/user/list-users.route.ts
+++ src/infrastructure/api/express/routes/user/list-users.route.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import { listUsersController } from '../../../controllers/user.controller';
-import { checkAction } from '../../../../../middlewares/auth.middleware';
 
 const router = express.Router();
 //vamos a quitar el checkAction router.get('/list', checkAction('viewAllUsers'), listUsersController);
